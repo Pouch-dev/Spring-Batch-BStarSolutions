c58ff5d4dad1d6c07955211b39b008ad172b1f3d
Pouch-dev <diemphuoc5555@gmail.com>
2021-09-27 18:49:38 +0700

    diemphuoc_update_27/09/2021

--- spring-batch/src/main/java/com/example/springbatch/model/UserManagement.java
+++ spring-batch/src/main/java/com/example/springbatch/model/UserManagement.java
@@ -4,7 +4,7 @@ import javax.persistence.*;
 import java.util.Date;
 
 @Entity
-@Table(name = "UserManagement")
+//@Table(name = "UserManagement")
 public class UserManagement {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
